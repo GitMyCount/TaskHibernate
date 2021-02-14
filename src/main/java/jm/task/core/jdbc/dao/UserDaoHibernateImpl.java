4f180c24de9551a5d454ea027549421a2b312edd
user <dvd.off@mail.ru>
2021-02-15 00:09:05 +0700

    add commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,106 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    Util util = new Util();
+    SessionFactory factory;
+    Session session;
+    Transaction transaction;
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
+        factory = util.getFactory();
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS user " +
+                "(id INTEGER AUTO_INCREMENT, " +
+                " name VARCHAR(50), " +
+                " lastName VARCHAR (50), " +
+                " age INTEGER not NULL, " +
+                " PRIMARY KEY (id))")
+                .addEntity(User.class)
+                .executeUpdate();
+
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void dropUsersTable() {
+        factory = util.getFactory();
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        session.createSQLQuery("DROP TABLE IF EXISTS user")
+                .addEntity(User.class)
+                .executeUpdate();
+
+        transaction.commit();
+        session.close();
+        factory.close();
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        User user = new User(name, lastName, age);
+        session.save(user);
+        System.out.println("User с именем – " + name + " добавлен в базу данных");
+
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public void removeUserById(long id) {
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        User user = session.get(User.class, id);
+        session.delete(user);
+
+        transaction.commit();
+        session.close();
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        List<User> users = session.createQuery("from User").getResultList();
+
+        transaction.commit();
+        session.close();
+        return users;
+
     }
 
     @Override
     public void cleanUsersTable() {
+        session = factory.openSession();
+        transaction = session.beginTransaction();
+
+        session.createQuery("delete User").executeUpdate();
+
+        transaction.commit();
+        session.close();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,48 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    String user = "root";
+    String password = "root";
+    String url = "jdbc:mysql://localhost:3306/bzdnusers";
+    String driver = "com.mysql.cj.jdbc.Driver";
+    Connection connection = null;
+
+    public Connection getConnection() throws ClassNotFoundException {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            return connection;
+        }
+        return connection;
+    }
+
+    public SessionFactory getFactory() {
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty(Environment.HBM2DDL_AUTO,"update");
+        properties.setProperty(Environment.DRIVER, driver);
+        properties.setProperty(Environment.USER, user);
+        properties.setProperty(Environment.PASS, password);
+        properties.setProperty(Environment.URL, url);
+
+        Configuration configuration = new Configuration();
+        SessionFactory factory = configuration
+                .setProperties(properties)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+        return factory;
+    }
 }
